a341fefd81741ebb38309bb5dce51490edfae8a4
tyler <tyleroar@gmail.com>
2020-03-22 15:52:21 -0400

    update

--- towns.py
+++ towns.py
@@ -1,5 +1,6 @@
 #colors 1-5, will 
 from enum import Enum
+import sys
 import random
 class Color(Enum):
   DISCARD = 0
@@ -40,6 +41,10 @@ class Deck:
     for color in range(1,6):
       for value in range(1,11):  #we're using 1 as the special handshake value
         self.cards.append(Card(color,value))
+        if value==1: ##need 3 handshakes
+          self.cards.append(Card(color,value))
+          self.cards.append(Card(color,value))
+
   def insertCard(self,card):
     self.cards.append(card)
   def deleteCard(self,card):
@@ -62,6 +67,13 @@ class Deck:
       if x == card:
         return True
     return False
+    #used for counting number of handshakes
+  def countCard(self,card):
+    count=0
+    for x in self.cards:
+      if x == card:
+        count+=1
+    return count
   def __len__(self):
     return len(self.cards)
 def printGameBoard(communityDecks):
@@ -73,7 +85,10 @@ def printGameBoard(communityDecks):
       if P1Deck.hasCard(card):
         #pass
       #  print Color(color).name
-        print str(number).ljust(7,' '),
+        value = number
+        if value==1:
+          value="HS*"+str(P1Deck.countCard(card))
+        print str(value).ljust(7,' '),
       else:
         print ''.ljust(7,' '),
 
@@ -91,26 +106,29 @@ def printGameBoard(communityDecks):
       value = 'HS'
     output=colorname[0]+str(value)
     print output.ljust(7,' '),
-  deck = communityDecks['DISCARD']
-  if len(deck)>0:
-    card = deck.cards[len(deck.cards)-1]
-    value = card.value
-    colorname = Color(card.color).name
-  else:
-    value = ''
-  if value == '':
-    output="Discard:Empty"
-  else:
-    if value == 1:
-      value = 'HS'
-    output="Discard:"+colorname[0]+str(value)
-  print output
+ # deck = communityDecks['DISCARD']
+ # if len(deck)>0:
+ #   card = deck.cards[len(deck.cards)-1]
+ #   value = card.value
+ #   colorname = Color(card.color).name
+ # else:
+ #   value = ''
+ # if value == '':
+ #   output="Discard:Empty"
+ # else:
+ #   if value == 1:
+ #     value = 'HS'
+ #   output="Discard:"+colorname[0]+str(value)
+ # print output
   #now need to print out P2Deck...upside down from how we did P1
   for number in range(1,11,1):
     for color in range (1,6):
       card = Card(Color(color).value,number)
       if P2Deck.hasCard(card):
-        print str(number).ljust(7,' '),
+        value = number
+        if value==1:
+          value="HS*"+str(P2Deck.countCard(card))
+        print str(value).ljust(7,' '),
       else:
         print ''.ljust(7,' '),
     print "\n"
@@ -122,6 +140,10 @@ def buildCommunityDeck():
   community['DISCARD'] = Deck()
   community['P1'] = Deck()
   community['P2'] = Deck()
+  community['mainDeck'] = Deck()
+  community['mainDeck'].build()
+  #mainDeck.revealAll()
+  community['mainDeck'].shuffle()
   #p1 and p2 community are the cards that they have already played!
   return community
 def mapinputtocolor(i):
@@ -164,12 +186,46 @@ def getCardValue(playerHand,color):
   print "Enter the card value to play"
   while value == None:
     value = int(raw_input())
-    if value >10 or value < 1:
+    if value >10 or value < -1:
       print "get card invalid selection, try again"
   card = Card(color.value,value)
   return card
 #def getPlayMove(playerHand):
  # return ""
+
+#this hould prompt player where they want to draw form, add it to their hand and remove it from wherever it is at
+
+def getDraw(playerName,playerHand,communityDeck,lastColor):
+  color = None
+  print "Enter the color you want to draw from or D to draw"
+  while color == None:
+    row = raw_input()
+    color = mapinputtocolor(row)
+    if color == None:
+      continue
+    if color == Color(0):
+      #this means they're drawing from the main deck
+      if len(communityDeck['mainDeck'])==0:
+        raise Exception("no cards left to draw from")
+      card = communityDeck['mainDeck'].cards.pop()
+      playerHand.insertCard(card)
+    else:
+      if color == lastColor:
+        print "you can't pick up from the pile you just discarded to!"
+        color = None
+        continue
+      #they're picking up from the community card pile
+      
+      if len(communityDeck[color.name])==0:
+        print "no cards to pick up from that pile!"
+        color=None#force ot got through the loop again
+      else:
+        card=communityDeck[color.name].cards.pop()
+        playerHand.insertCard(card)
+   # if communityDeck['DISCARD']
+    if color == None:
+      print "Invalid, selection, try again"
+  
 def getMove(playerName,playerHand,communityDeck):
   print playerName
   color = getMoveType()
@@ -189,38 +245,47 @@ def getMove(playerName,playerHand,communityDeck):
     card = getCardValue(playerHand,color)
   if discard==True:
     playerHand.deleteCard(card)
-    communityDeck['DISCARD'].insertCard(card)
+    communityDeck[color.name].insertCard(card)
   elif discard == False:
     print "playing card: {}".format(card)
     communityDeck[playerName].insertCard(card)
     playerHand.deleteCard(card)
-      
-    
+  getDraw(playerName,playerHand,communityDeck,color)
+def printGameSummary(deck):
+  pass
+  #need to score both players hands
+ # deck['P1']
+ # deck['P2']
+#game is over when 0 cards left in the main deck
+def gameOver(deck):
+  return len(deck)==0
 def main():
-  mainDeck = Deck()
-  mainDeck.build()
-  mainDeck.shuffle()
-  #a.revealAll()
+
   P1Hand = Deck()
   P2Hand = Deck()
   community = buildCommunityDeck()
   for i in range(0,8):
-    P1Hand.insertCard(mainDeck.cards.pop())
-    P2Hand.insertCard(mainDeck.cards.pop())
+    P1Hand.insertCard(community['mainDeck'].cards.pop())
+    P2Hand.insertCard(community['mainDeck'].cards.pop())
   print "Player 1 cards"
   P1Hand.revealAll()
   print "player 2 cards"
   P2Hand.revealAll()
   print "Remaining:"
-  mainDeck.revealAll()
+ # mainDeck.revealAll()
   b = Card(Color.RED.value,10)
   while True:
     print "Player 1 cards"
     P1Hand.revealAll()
     printGameBoard(community)
     getMove("P1",P1Hand,community)
+    if gameOver(community['mainDeck']):
+      break
     P2Hand.revealAll()
     printGameBoard(community)
     getMove("P2",P2Hand,community)
+    if gameOver(community['mainDeck']):
+      break
+  printGameSummary(community)
 if __name__=="__main__":
   main()
